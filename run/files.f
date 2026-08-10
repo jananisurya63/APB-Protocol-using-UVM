c62f5a42053fbe41427b7fdb357dc822bf970a72
JANANI SURYA <jananisurya63@gmail.com>
2026-08-10 09:22:16 +0530

    Create files.f

---
+++ run/files.f
@@ -0,0 +1,15 @@
++incdir+/home/dvft1101/final_project/apb_uvm/verification/agent/
++incdir+/home/dvft1101/final_project/apb_uvm/verification/env/
++incdir+/home/dvft1101/final_project/apb_uvm/verification/test/
++incdir+/home/dvft1101/final_project/apb_uvm/verification/tb/
+
+/home/dvft1101/final_project/apb_uvm/verification/tb/apb_interface.sv
+
+
+/home/dvft1101/final_project/apb_uvm/verification/agent/apb_agent_package.svp
+/home/dvft1101/final_project/apb_uvm/verification/env/apb_env_package.svp
+/home/dvft1101/final_project/apb_uvm/verification/test/apb_test_package.svp
+
+
+/home/dvft1101/final_project/apb_uvm/design/apb_slave_design.sv
+/home/dvft1101/final_project/apb_uvm/verification/tb/apb_top.sv
